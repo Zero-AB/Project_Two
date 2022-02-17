974fe0b3dc7639837685bef7c3a5353f37d76de2
Jacob-Monks <97700113+Jacob-Monks@users.noreply.github.com>
2022-02-17 12:32:25 -0500

    Username and Password system works (mostly)

--- Project_two/src/main/scala/login.scala
+++ Project_two/src/main/scala/login.scala
@@ -8,12 +8,12 @@ object login {
     print("Password:\t")
     val pass = readLine()
     val df4 = spark.read.format("csv").option("header", "true").load("input/user_data.csv")
-    val dfuser = df4.select("*").where(df4("Username")===user).show()
-    val a = dfuser.
-//    if (user == dfuser & pass == dfuser) {
-//
-//    } else {
-//
-//    }
+    val dfuser = df4.select("*").where(df4("Username")===user)
+    if (user == dfuser.first.getString(0) & pass == dfuser.first.getString(1)) {
+      println("Welcome to the Climate Analysis Tool")
+    } else {
+      println("Username/Password is incorrect.")
+      UserLogin(spark)
+    }
   }
 }
